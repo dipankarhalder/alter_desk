9083eb7feea86b8f459868c921b18154f824f034
dipankarhalder <dipankar.appdev@gmail.com>
2025-01-17 13:36:41 +0530

    updated messages

--- backend/core/src/controllers/health.controller.js
+++ backend/core/src/controllers/health.controller.js
@@ -1,14 +1,16 @@
+const { StatusCodes } = require("http-status-codes");
+
 const { health_check } = require("../constant/msg.constant");
 
 const getHealthCheck = async (req, res) => {
   try {
-    return res.status(200).json({
-      status: 200,
+    return res.status(StatusCodes.OK).json({
+      status: StatusCodes.OK,
       msg: health_check.success_msg,
     });
   } catch (error) {
-    return res.status(500).json({
-      status: 500,
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      status: StatusCodes.INTERNAL_SERVER_ERROR,
       msg: health_check.error_msg,
       error: error.message,
     });
